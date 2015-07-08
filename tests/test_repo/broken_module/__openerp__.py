df2c03b4865c26096c50808fe2086af46f86476d
Sudhir Arya <sudhir.arya@serpentcs.com>
2015-07-08 11:17:47 +0530

    Added sudo true in travis yml

--- tests/test_repo/broken_module/__openerp__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-    'name': 'Broken module for tests',
-    'version': '1.0',
-    'depends': ['base'],
-    'data': [],
-    'test': ['test.yml'],
-    'installable': True,
-}
--- tests/test_repo/second_module/__openerp__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-    'name': 'Second empty module for tests',
-    'version': '1.0',
-    'depends': [
-        'base'],
-}
